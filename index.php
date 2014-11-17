f0820219cfa3040c704fa98b73faea446a5d8639
Nathan Kane <kanenathan213@gmail.com>
2014-11-16 19:59:33 -0800

    header partial

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 <html ng-app="FinancialFreedom">
 
     <head>
+        
+        <link rel="stylesheet" type="text/css" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="stylesheets/output/screen.css">
-    
+
         <script type="text/javascript" src="vendor/javascript/angular.min.js"></script>
         <script type="text/javascript" src="javascript/retirement_calculator_module.js"></script>
         <script type="text/javascript" src="javascript/app.js"></script>
@@ -13,8 +15,14 @@
     </head>
     
     <body>
+
+        <div class="header navbar navbar-default">
+            <p class="productName">Beanstalk</p>
+            <p class="tagLine">Countdown to retirement</p>
+        </div>
+
         <div ng-controller="RetirementCalculatorController" class="first-div">
-            <?php echo "this is php" ?>
+            
             <br><br>
             
             <form ng-submit="calculateYearsToRetirement()">
@@ -27,6 +35,9 @@
             <br><br>
             
             Time to retire: <span class="retirement-years" ng-bind="retirement.years_to_retirement + ' years'"></span>
+
+            <br>
+            <?php echo "this is php" ?>
             
         </div>
     </body>
