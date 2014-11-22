2ae645d815ff4f3bd7e3597d73b82faacb39cff3
Nathan Kane <kanenathan213@gmail.com>
2014-11-22 12:25:39 -0800

    added font, updated bower with d3

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <html ng-app="FinancialFreedom">
 
     <head>
+        <link href='http://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" type="text/css" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="stylesheets/output/screen.css">
         <script type="text/javascript" src="bower_components/d3/d3.min.js"></script>
