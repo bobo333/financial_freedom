2ab77a7b8960fd7638d2509c5cacdcdcbf65c5e7
Nathan Kane <kanenathan213@gmail.com>
2014-12-02 23:20:07 -0800

    consolidated meta tag

--- index.php
+++ index.php
@@ -4,8 +4,7 @@
 
     <head>
 
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
         <link href='http://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,600' rel='stylesheet' type='text/css'>
