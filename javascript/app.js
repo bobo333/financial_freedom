56090866154b3a63d3a75a0eb9572ceb96619761
Nathan Kane <kanenathan213@gmail.com>
2014-12-02 18:52:41 -0800

    hide graph if years > 100

--- javascript/app.js
+++ javascript/app.js
@@ -101,10 +101,13 @@ FinancialFreedom.controller('TimeToRetirementController', ['$scope', 'Retirement
     
     var retirement_data = RetirementCalculatorService.calculateMonthsToRetirement();
     var months_to_retirement = retirement_data['months'];
-    $scope.retirement.years_to_retirement = Math.floor(months_to_retirement / 12);
+    var years_to_retirement = Math.floor(months_to_retirement / 12);
+    $scope.retirement.years_to_retirement = years_to_retirement;
     $scope.retirement.months_to_retirement = months_to_retirement % 12;
     
-    createRetirementGraph(retirement_data['data_to_graph']);
+    if (years_to_retirement < 100) {
+        createRetirementGraph(retirement_data['data_to_graph']);
+    }
     
     $(window).resize(function() {
         createRetirementGraph(retirement_data['data_to_graph']);
