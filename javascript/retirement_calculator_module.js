1290437fae56e684655dc906373b44d081480188
bobo333 <redsox333@gmail.com>
2015-01-31 13:07:28 -0800

    Planning for refactor of retirement module

--- javascript/retirement_calculator_module.js
+++ javascript/retirement_calculator_module.js
@@ -12,30 +12,37 @@ RetirementCalculatorModule.service('RetirementCalculatorService', function() {
     var monthly_income;
     var monthly_expenses;
     
+    // retirement
     this.getMonthlyIncome = function() {
         return monthly_income;
     };
     
+    // retirement
     this.setMonthlyIncome = function(new_monthly_income) {
         monthly_income = new_monthly_income;
     };
     
+    // retirement
     this.getTotalAssets = function() {
         return total_assets;
     };
     
+    // retirement
     this.setTotalAssets = function(new_total_assets) {
         total_assets = new_total_assets;
     };
     
+    // retirement
     this.getMonthlyExpenses = function() {
         return monthly_expenses;
     };
     
+    // retirement
     this.setMonthlyExpenses = function(new_monthly_expenses) {
         monthly_expenses = new_monthly_expenses;
     };
 
+    // retirement
     this.calculateRetirementInfo = function() {
         var retirement_data = this.initialRetirementData();
         calculateRetirementTrajectory(retirement_data);
@@ -49,6 +56,7 @@ RetirementCalculatorModule.service('RetirementCalculatorService', function() {
         return retirement_data;
     };
     
+    // retirement
     var calculateRetirementTrajectory = function(retirement_data) {
         if (canRetire(retirement_data)) {
             return setImmediateRetirement(retirement_data);
@@ -66,11 +74,13 @@ RetirementCalculatorModule.service('RetirementCalculatorService', function() {
         }
     };
     
+    // retirement
     var addPointsForIntersection = function(retirement_data) {
         var points_for_intersection = retirement_data.graph_points.slice(-2);
         retirement_data.points_for_intersection = points_for_intersection;
     };
     
+    // retirement
     var padGraphData = function(retirement_data) {
         var points_to_add = retirement_data.graph_points.length / 2;
     
@@ -79,6 +89,7 @@ RetirementCalculatorModule.service('RetirementCalculatorService', function() {
         }
     };
     
+    // retirement?
     var addIntersectionPoint = function(retirement_data) {
         var points_for_intersection = retirement_data.points_for_intersection;
         var intersection_point = calculateIntersectionPoint(points_for_intersection);
@@ -86,6 +97,7 @@ RetirementCalculatorModule.service('RetirementCalculatorService', function() {
         retirement_data.intersection_point = intersection_point;
     };
     
+    // geometry?
     var calculateIntersectionPoint = function(points_for_intersection) {
         var before_values = points_for_intersection[0];
         var after_values = points_for_intersection[1];
@@ -102,6 +114,7 @@ RetirementCalculatorModule.service('RetirementCalculatorService', function() {
         return formatIntersectionPoint(intersection_date, y_intersection);
     };
     
+    // retirement
     var canRetire = function(retirement_data) {
         var investment_amount = retirement_data.total_assets;
         var annual_expenses = retirement_data.monthly_expenses * 12;
@@ -109,10 +122,12 @@ RetirementCalculatorModule.service('RetirementCalculatorService', function() {
         return WITHDRAWAL_RATE * investment_amount >= annual_expenses;
     };
     
+    // retirement
     var setImmediateRetirement = function(retirement_data) {
         retirement_data.can_retire_immediately = true;
     };
     
+    // retirement
     var addNextGraphPoint = function(retirement_data) {
         var month_date = getNextMonthDate(retirement_data);
         var monthly_withdrawal_limit = calculateMonthlyWithdrawalLimit(retirement_data.total_assets);
@@ -127,6 +142,7 @@ RetirementCalculatorModule.service('RetirementCalculatorService', function() {
         }
     };
     
+    // retirement
     var updateTotalAssets = function(retirement_data) {
         var total_assets = retirement_data.total_assets;
         var pay = retirement_data.monthly_income;
@@ -136,6 +152,7 @@ RetirementCalculatorModule.service('RetirementCalculatorService', function() {
         retirement_data.total_assets = addInterest(total_assets, interest_rate) + pay - expenses;
     };
     
+    // retirement
     var updateMonthlyExpenses = function(retirement_data) {
         var expenses = retirement_data.monthly_expenses;
         var inflation_rate = retirement_data.monthly_inflation_rate;
@@ -143,16 +160,19 @@ RetirementCalculatorModule.service('RetirementCalculatorService', function() {
         retirement_data.monthly_expenses = addInterest(expenses, inflation_rate);
     };
     
+    // retirement
     var updateMonthlyIncome = function(retirement_data) {
         var monthly_income = retirement_data.monthly_income;
     
         retirement_data.monthly_income = addInterest(monthly_income, INCOME_INCREASE_RATE);
     };
     
+    // interest
     var addInterest = function(original_total, interest_rate) {
         return original_total * (1 + interest_rate);
     };
     
+    // retirement
     var formatGraphData = function(date, expenses, withdraw_limit) {
         return {
             date: date,
@@ -161,6 +181,7 @@ RetirementCalculatorModule.service('RetirementCalculatorService', function() {
         }
     };
     
+    // geometry
     var formatIntersectionPoint = function(intersection_date, y_intersection) {
         return {
             x: intersection_date,
@@ -168,6 +189,7 @@ RetirementCalculatorModule.service('RetirementCalculatorService', function() {
         };
     };
     
+    // geometry
     var formatLineData = function(x, y, m) {
         return {
             x: x,
@@ -176,10 +198,12 @@ RetirementCalculatorModule.service('RetirementCalculatorService', function() {
         };
     };
     
+    // retirement
     var incrementMonthCount = function(retirement_data) {
         retirement_data.months++;
     };
     
+    // retirement?
     var getNextMonthDate = function(retirement_data) {
         date = new Date();
         months_in_future = retirement_data.graph_points.length;
@@ -188,6 +212,7 @@ RetirementCalculatorModule.service('RetirementCalculatorService', function() {
         return date;
     };
     
+    // retirement?
     var getIntersectionDate = function(start_date, x_intersection) {
         var intersection_date = new Date(start_date.getTime());
         var days_to_add = Math.round(x_intersection);
@@ -197,26 +222,32 @@ RetirementCalculatorModule.service('RetirementCalculatorService', function() {
         return intersection_date;
     };
     
+    // retirement
     var calculateMonthlyWithdrawalLimit = function(total_assets) {
         return WITHDRAWAL_RATE * total_assets / 12;
     };
     
+    // retirement?
     var newYear = function(months) {
         return months % 12 === 0;
     };
     
+    // geometry
     var calculateXIntersection = function(line1, line2) {
         return (line1.m * line1.x - line2.m * line2.x + line2.y - line1.y) / (line1.m - line2.m);
     };
     
+    // geometry
     var calculateYIntersection = function(line1, line2) {
         return (line1.m * line2.m * (line2.x - line1.x) + line1.y * line2.m - line2.y * line1.m) / (line2.m - line1.m)
     };
     
+    // geometry
     var calculateSlope = function(y1, y2, dx) {
         return (y2 - y1) / dx;
     };
     
+    // retirement?
     var calculateDaysBetween = function(date1, date2) {
         var one_day=1000*60*60*24;
         var date1_ms = date1.getTime();
@@ -227,10 +258,12 @@ RetirementCalculatorModule.service('RetirementCalculatorService', function() {
         return Math.round(difference_ms/one_day); 
     };
     
+    // retirement?
     var addDaysToDate = function(date, days) {
         date.setDate(date.getDate() + days);
     };
     
+    // retirement
     this.initialRetirementData = function() {
         var MONTHLY_INFLATION_RATE = this.calculatePeriodInterestRate(INFLATION_RATE, 12);
         var MONTHLY_GROWTH_RATE = this.calculatePeriodInterestRate(GROWTH_RATE, 12);
@@ -247,6 +280,7 @@ RetirementCalculatorModule.service('RetirementCalculatorService', function() {
         };
     };
     
+    // interest rates
     this.calculatePeriodInterestRate = function(overall_rate, number_of_periods) {
         base = overall_rate + 1;
         exponent = 1 / number_of_periods;
