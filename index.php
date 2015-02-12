6d3818914aa9268af363f2a0c7a0a4fce0f1c233
Nathan Kane <kanenathan213@gmail.com>
2015-02-12 08:00:12 -0800

    52-moved ga setup code in index to own file

--- index.php
+++ index.php
@@ -29,7 +29,8 @@
         <script type="text/javascript" src="javascript/geometry_service.js"></script>
         <script type="text/javascript" src="javascript/retirement_calculator_service.js"></script>
         <script type="text/javascript" src="javascript/create_retirement_graph_service.js"></script>
-        
+        <script type="text/javascript" src="javascript/google-analytics.js"></script>
+
 
         <title>Plenti: Financial freedom for all</title>
     </head>
@@ -76,13 +77,4 @@
         
         <div ng-view class="view-animate"></div>
     </body>
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-59504141-1', 'auto');
-
-    </script>
 </html>
