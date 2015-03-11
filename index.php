c4f078d21c0037791041530f508c32181ceecca6
Nathan Kane <kanenathan213@gmail.com>
2015-03-10 23:40:36 -0700

    57-popover placement, intersection box padding

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
         <link rel="icon" type="image/png" href="img/favicon.ico">
         
         <script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
-        <!-- <script type="text/javascript" src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script> -->
         <script type="text/javascript" src="bower_components/d3/d3.min.js"></script>
         <script type="text/javascript" src="bower_components/angular/angular.js"></script>
         <script type="text/javascript" src="bower_components/angular-animate/angular-animate.js"></script>
