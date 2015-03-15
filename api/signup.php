305b644a068b22ca4e0a94885f9bb937ebc51650
bobo333 <redsox333@gmail.com>
2015-03-15 16:42:03 -0700

    35 - adding created_at column to user table

--- api/signup.php
+++ api/signup.php
@@ -5,3 +5,18 @@
     );
     
     echo json_encode($output);
+
+
+    # check if logged in
+    # check if email already in use
+    # check if password meets requirements
+    # check other params are present
+        # monthly income
+        # total assets
+        # monthly expenses
+        # income growth rate
+        # investment growth rate
+        # expense growth rate
+
+    # create account and log user in
+    # return success true
