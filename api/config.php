7c5fe251f3ed7c88f7d01f82cd051d5d33d24e14
bobo333 <redsox333@gmail.com>
2015-04-19 20:37:31 -0700

    35 - adding successful log in test, adding updated seed file for db tests

--- api/config.php
+++ api/config.php
@@ -1,6 +1,6 @@
 <?php
 
 $config = [
-    // 'db_name' => 'financial_freedom_test'
-    'db_name' => 'financial_freedom'
+    'db_name' => 'financial_freedom_test'
+    // 'db_name' => 'financial_freedom'
 ];
