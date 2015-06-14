3a88fc4d36cf503f0faaad5c0899e7d4f0989b67
bobo333 <redsox333@gmail.com>
2015-06-14 15:23:44 -0700

    70 - adding test for no new_password when changing password

---
+++ tests/api/change-password/NoNewPasswordCept.php
@@ -0,0 +1,18 @@
+<?php 
+    $I = new ApiTester($scenario);
+
+    $I->wantTo('Change password with no old_password');
+    $I->sendPOST('/login.php', array('email' => 'test1@test.com', 'password' => '123'));
+    $I->seeResponseContains('{"success":true,"errors":[]}');
+    $I->sendPOST('/change-password.php', array("old_password" => "123456"));
+    $I->seeResponseCodeIs(200);
+    $I->seeResponseIsJson();
+
+    $expected_response = [
+        "success" => FALSE,
+        "errors" => ["new_password required."]
+    ];
+
+    $I->seeJsonResponseContains($expected_response);
+    $I->seeInDatabase('users', array('email' => 'test1@test.com', 'password_and_salt' => '$2y$10$Z7txCQXh6LRAxnpMnV.xTevdYWRQ3P7MPTn8hLgS/DcvE6bBiKOM2'));
+?>
