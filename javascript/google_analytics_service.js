981fbc567a0beb9e0696efee8c58a3bf34b11f91
bobo333 <redsox333@gmail.com>
2015-09-20 14:21:24 -0700

    Conditionally sending Google Analytics events

--- javascript/google_analytics_service.js
+++ javascript/google_analytics_service.js
@@ -1,6 +1,7 @@
 FinancialFreedom.service('GoogleAnalyticsService', ['$rootScope', '$location', '$window', function($rootScope, $location, $window) {
-
 	$rootScope.$on('$routeChangeSuccess', function(event) {
-    	$window.ga('send', 'pageview', { page: $location.url() });
+        if (window.location.host !== 'localhost') {
+            $window.ga('send', 'pageview', { page: $location.url() });
+        }
 	});
 }]);
