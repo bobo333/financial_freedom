c510d9a08631a5e1d0512217b578ceef93462368
Steven Cipriano <redsox333@gmail.com>
2020-04-19 00:07:38 -0700

    in a working state

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
+# Installing
+
+```
+npm install
+npm install -g bower
+bower install
+npm install -g karma-cli
+npm install karma
+npm install -g jasmine
+npm install -g grunt
+npm install grunt-contrib-compass --save-dev
+sudo apt install autogen
+sudo apt install autoconf
+sudo apt install ruby-full
+sudo gem install sass
+sudo gem install compass
+grunt compass:dist
+bower install angular-resource
+```
+
+
 # API Documentation #
 
 Last Updated: 5/11/2015
--- javascript/google_analytics_service.js
+++ javascript/google_analytics_service.js
@@ -1,7 +1,7 @@
-FinancialFreedom.service('GoogleAnalyticsService', ['$rootScope', '$location', '$window', function($rootScope, $location, $window) {
-	$rootScope.$on('$routeChangeSuccess', function(event) {
-        if (window.location.host !== 'localhost') {
-            $window.ga('send', 'pageview', { page: $location.url() });
-        }
-	});
-}]);
+// FinancialFreedom.service('GoogleAnalyticsService', ['$rootScope', '$location', '$window', function($rootScope, $location, $window) {
+// 	$rootScope.$on('$routeChangeSuccess', function(event) {
+//         if (window.location.host !== 'localhost') {
+//             $window.ga('send', 'pageview', { page: $location.url() });
+//         }
+// 	});
+// }]);
